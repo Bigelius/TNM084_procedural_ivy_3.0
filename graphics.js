124780d9fba472c5920ea6b44ec72424c7e8279b
Amanda Bigelius <44239216+Bigelius@users.noreply.github.com>
2023-10-06 11:40:31 +0200

    Graphics.js cleanup
    - Cleaned unused variables
    - Added comments
    - Made code pretty

--- graphics.js
+++ graphics.js
@@ -1,13 +1,12 @@
-/************************************
-    BLANDADE STANDARD-GREJER
-*************************************/
-//Importera L-sträng
+//Importerade funktioner
 import {sentence} from "./lsystem.js";
 import {getColor} from "./rules.js";
-//import {makeVoxelMatrix} from "./voxels.js";
-console.log(sentence);
+import {makeVoxelMatrix} from "./voxels.js";
+//console.log(sentence);
 
-//var theMatrix = makeVoxelMatrix(100);
+//Globala variabler
+var max_val = 30;   //Hur många voxlar i xyz
+var theMatrix = makeVoxelMatrix(max_val); //Skapar voxel-matris med bools
 var scene = new THREE.Scene();
 
 //Kameror
@@ -23,7 +22,7 @@ var scene = new THREE.Scene();
 }
     
 //Blandat
-
+{
     //Set up the renderer, uses pespective renderer
     var renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setClearColor("#e5e5e5"); //the color of the background
@@ -58,49 +57,46 @@ var scene = new THREE.Scene();
     //This function is so that the mesh (the object) wont scale with us when we change the size 
     //of the window. It wont stretch. Creates a loop that draw the scene everytime the
     //the screen is refreshed.
+}
+// HEMMASNICKRADE VEKTOROPERATORER  (obs, kan inte gömmas under {})
 
+function subVec3(a, b) {
+ return new THREE.Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+function addVec3(a, b) {
+ return new THREE.Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+function absVec3(a) {
+  var sum = (a.x*a.x) + (a.y*a.y) + (a.z*a.z);
+  var res = Math.sqrt(sum);
+  return res;
+}
+function pointDist(a, b){
+    var diff = subVec3(a,b);
+    return absVec3(diff);
+}
 
-    /***    Några gamla vektoroperatorer (abs, '-') ***/   
-    function subVec3(a, b) {
-     return new THREE.Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-    function addVec3(a, b) {
-     return new THREE.Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-    function absVec3(a) {
-      var sum = (a.x*a.x) + (a.y*a.y) + (a.z*a.z);
-      var res = Math.sqrt(sum);
-      return res;
-    }
-    function pointDist(a, b){
-        var diff = subVec3(a,b);
-        return absVec3(diff);
-    }
-    
-    /************************************************
-    *       Börjar skapa saker
-    ************************************************/
+/************************************************
+*       Börjar skapa saker
+************************************************/
 
-    //RITAR REFERENSLÅDA
-    /*{
+//REFERENSLÅDA
+/*{
     var gHouse = new THREE.BoxGeometry(2,2,2);
     var mHouse = new THREE.MeshLambertMaterial({color: 0xff0066, transparent: true, opacity: 1, visible: true});
     var houseMesh = new THREE.Mesh(gHouse, mHouse);
     houseMesh.position.set(-5, 0, 0);
     scene.add(houseMesh);
-    }*/
-
-
+}*/
 
 //SPECS FÖR SEGMENT 
 {
-var sum = 0;
-var radiusBottom = 0.1;
-var radiusTop = 0.1;
-var segmentLength = 1;
-var rootCoord = new THREE.Vector3( 0, 0, 0 );
+    var sum = 0;
+    var radiusBottom = 0.1;
+    var radiusTop = 0.1;
+    var segmentLength = 1;
+    var coordIterator = new THREE.Vector3( 0, 0, 0 ); //Global startposition. Updates with every segment to backtrack branches
 }
-
 
 
 function createTreeSegment(pos, rot, char, lenght){
@@ -131,11 +127,11 @@ function createTreeSegment(pos, rot, char, lenght){
     
     var temp = new THREE.LineSegments().copy(line);
     temp.translateY(lenght/2);
-    temp.getWorldPosition(rootCoord); //Sparar ny coordinat i rootCoord
+    temp.getWorldPosition(coordIterator); //Sparar ny coordinat i rootCoord
 }
 
      
-//Starts on the first bracket, ends on the last one. Returns GLOBAL index
+//Starts on the first bracket, ends on the last one. Returns GLOBAL(!!) index
 function getEndIndexOfBranch(start_INDEX){
     var counter = 0;
     var i = start_INDEX;
@@ -156,18 +152,13 @@ function getEndIndexOfBranch(start_INDEX){
 }
 
 //Drawing the tree
-//Ska man ta bort c_rot helt?
 //Vad gör alla de här?
-var tempVec = new THREE.Vector3(0, 0, 0)
-var starts = [];
-var NEXT_COORD;
 
 function createTree(start_INDEX, end_INDEX, startPosition, c_rot, segLen){
     // Iterator som ändras. 
     
     var current_index = start_INDEX;
     var oldPosition = startPosition; //Sparar pos-koordinaten som en temp
-    var dustyOldVec = tempVec;
     
     //Loop over local main branch segments
     //While we're not at the end of the local straight branch...
@@ -175,42 +166,20 @@ function createTree(start_INDEX, end_INDEX, startPosition, c_rot, segLen){
         //Räknar...
         if(sentence[current_index] == "[") {
             
-            var lbi = getEndIndexOfBranch(current_index); //Last bracket of new branch!
+            var lastBranchIndex = getEndIndexOfBranch(current_index); //Last bracket of new branch!
+            var prevPosition = new THREE.Vector3(coordIterator.x, coordIterator.y, coordIterator.z); 
+            var seg_rot = new THREE.Vector3(c_rot.x, c_rot.y + Math.PI/5, c_rot.z + Math.PI/5); //New initial rotation for branch
             
-
-            var wtf = new THREE.Vector3(rootCoord.x, rootCoord.y, rootCoord.z);
-            starts.push(wtf) //Latest saved initial position as a temp
-            
-            /* Hypotes: Indexteringen i starts blir fel */
-            
-            var b_rot = new THREE.Vector3(c_rot.x, c_rot.y + Math.PI/5, c_rot.z + Math.PI/5); //New initial rotation for branch
-            
-            createTree(current_index + 1, lbi, rootCoord, b_rot, segLen); //Make new branch with the new initial specs
-            /* Over here the rootCoord have updated to top of the new tree above */
-            
-            //Updates the index 
-            current_index = lbi; //Go to next index after recursive branch
-            
+            createTree(current_index + 1, lastBranchIndex, coordIterator, seg_rot, segLen); //Make new branch with the new initial specs
+            current_index = lastBranchIndex; //Go to next index after recursive branch
         }
-
-        else if(sentence[current_index] != "]" && sentence[current_index] != "-" && sentence[current_index] != "+"){
-            //At start, segments render in a single center point
-            
-            createTreeSegment(rootCoord, c_rot, sentence[current_index], segmentLength);
-        }
-        
-        else {
-
-            var stamCoord = starts[starts.length-1]
-            rootCoord.x = stamCoord.x;
-            rootCoord.y = stamCoord.y;
-            rootCoord.z = stamCoord.z;
-            starts.pop()
+        //Draw segment if not '[' or ']'. CoordIterator is the top of last segment
+        else if(sentence[current_index] != "]"){
+            createTreeSegment(coordIterator, c_rot, sentence[current_index], segmentLength);
         }
         ++current_index;
     }
-    tempVec = dustyOldVec;
-    rootCoord = oldPosition; 
+    coordIterator = oldPosition; //Move iterator to the previous branch (where we left it to make new branch)
 }
 // Här måste man slänga in startposition m.a.p. objektet man lägger in, 
 // eller i mitten av bounding boxen (50, 0, 50);
@@ -220,7 +189,7 @@ function createTree(start_INDEX, end_INDEX, startPosition, c_rot, segLen){
 
 
 /*Ritar voxlar*/
-var max_val = 30;   //Hur många voxlar i xyz
+
 var sphereCenter = new THREE.Vector3(4,4,4);
 var cLen = 1/4;
 //Ritar en voxel
