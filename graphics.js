c2fbc69fae832049c556d0755b820e16b07c52be
jonimode <45713029+jonimode@users.noreply.github.com>
2023-10-13 15:23:03 +0200

    ???

--- graphics.js
+++ graphics.js
@@ -12,7 +12,7 @@ var sentence = [];
     var girthStarts = [];
     var girthShrinkRates = [];
 
-    
+
     var segmentLength = 2;
     var rootCoord = new THREE.Vector3(0, 0, 0);
 }
@@ -38,21 +38,21 @@ function absVec3(a) {
     var res = Math.sqrt(sum);
     return res;
 }
-function pointDist(a, b){
-    let diff = subVec3(a,b);
+function pointDist(a, b) {
+    let diff = subVec3(a, b);
     return absVec3(diff);
 }
-function coord2Index(coord){
-	if(coord.x > x_side - 1){
-		console.log("X COORDINATE OUT OF BOUNDS")
-	}
-	else if(coord.y > y_side - 1){
-		console.log("Y COORDINATE OUT OF BOUNDS")
-	}
-	else if(coord.z > z_side - 1){
-		console.log("Z COORDINATE OUT OF BOUNDS")
-	} 
-	else return new THREE.Vector3(floor(coord.x), floor(coord.y), floor(coord.z));
+function coord2Index(coord) {
+    if (coord.x > x_side - 1) {
+        console.log("X COORDINATE OUT OF BOUNDS")
+    }
+    else if (coord.y > y_side - 1) {
+        console.log("Y COORDINATE OUT OF BOUNDS")
+    }
+    else if (coord.z > z_side - 1) {
+        console.log("Z COORDINATE OUT OF BOUNDS")
+    }
+    else return new THREE.Vector3(floor(coord.x), floor(coord.y), floor(coord.z));
 }
 /*****************************************
 *    MATRISEN OCH INITIALA VOXLAR
@@ -61,93 +61,93 @@ function coord2Index(coord){
 
 var matrixSize = 20;
 var possibleStartPositions = [];
-var sphereMid = new THREE.Vector3(matrixSize/2, 1, matrixSize/2);
+var sphereMid = new THREE.Vector3(matrixSize / 2, 1, matrixSize / 2);
 var radIn = 7;
 var radOut = 10;
 //'False' = tom voxel
 //OBS: Index 0 - 19!!
-var voxelMid = new THREE.Vector3(0,0,0);
+var voxelMid = new THREE.Vector3(0, 0, 0);
 
-function drawVoxel(pos){
+function drawVoxel(pos) {
 
     var g = new THREE.CubeGeometry(1, 1, 1);
-    var edges = new THREE.EdgesGeometry( g );
+    var edges = new THREE.EdgesGeometry(g);
 
-    var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x000000 } ) );
-    var m = new THREE.MeshLambertMaterial({color: 0xfc0324});
+    var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));
+    var m = new THREE.MeshLambertMaterial({ color: 0xfc0324 });
     m.transparent = true;
     m.opacity = 0.1;
     var seg = new THREE.Mesh(g, m);
 
     seg.position.set(pos.x, pos.y, pos.z);
-    
+
     line.position.set(pos.x, pos.y, pos.z);
-    
+
     scene.add(line);
     scene.add(seg);
 }
 
-function makeVoxelMatrix(size){
+function makeVoxelMatrix(size) {
     let i = 0;
     let j = 0;
     let k = 0;
-    
+
     let mat_x = [Boolean];
-    while(i < size){
+    while (i < size) {
         mat_x.push(false);
         ++i;
     }
     var temp = mat_x;
     var mat_xy = [temp];
-    while(j < size){
+    while (j < size) {
         var temp2 = mat_x;
         mat_xy.push(temp2);
         ++j;
     }
     var temp3 = mat_xy;
     var mat_xyz = [temp3];
-    while(k < size){
+    while (k < size) {
         var temp4 = mat_xy;
         mat_xyz.push(temp4);
         ++k;
     }
-    
+
     i = 0;
     j = 0;
     k = 0;
     //Define initial empty voxels
-    while(i < size) {
+    while (i < size) {
         j = 0;
 
-        while(j < size) {
+        while (j < size) {
             k = 0;
-            
-            while(k < size) {
+
+            while (k < size) {
                 //Find center of voxel
                 voxelMid = new THREE.Vector3(i + 0.5, j + 0.5, k + 0.5);
                 //Ritar hela voxel-fältet
                 //drawVoxel(voxelMid);
-                
+
                 var dist = pointDist(voxelMid, sphereMid);
                 //Is the voxel on floor and outside sphere?
-                if(j == 0 && dist > radIn){
+                if (j == 0 && dist > radIn) {
                     mat_xyz[i][j][k] = true;
                     //Ritar marken
                     //drawVoxel(voxelMid);
                 }
                 //Is the voxel close to the sphere? (Inre radie < V < yttre radie)
-                else if(dist > radIn && dist < radOut){
+                else if (dist > radIn && dist < radOut) {
                     mat_xyz[i][j][k] = true;
-                    
+
                     //Ritar lager runt sfär
-                    //drawVoxel(voxelMid);
-                
+                    drawVoxel(voxelMid);
+
                     //Lägger till möjliga startpunkter i listan
                     //Tror inte den funkar för j=0 i.o.m. mid-kompensation
-                    if(j == 1){
-                        possibleStartPositions.push(new THREE.Vector3(i + 0.5, j, k+0.5));
+                    if (j == 1) {
+                        possibleStartPositions.push(new THREE.Vector3(i + 0.5, j, k + 0.5));
                         //Ritar startvoxlar
-                        //drawVoxel(voxelMid);
+                        drawVoxel(voxelMid);
                     }
                 }
                 k++;
@@ -156,30 +156,30 @@ function makeVoxelMatrix(size){
         }
         i++;
     }
-    
+
     return mat_xyz;
 }
 
 //Välj startposition från lista
 //Behöver parametervärde och längd på parametern
 //EJ KONTROLLERAD
-function initialStartPosition(paramValue, paramLenght){
-	scale = paramLenght / possibleStartPositions.lenght;  //Hur många param-värden per faktiskt startindex
-	normIndex = paramValue / scale;            //Hur många faktiska index skickar vi in från parameter
-	index = floor(normIndex);
-	return possibleStartPositions[index];
+function initialStartPosition(paramValue, paramLenght) {
+    scale = paramLenght / possibleStartPositions.lenght;  //Hur många param-värden per faktiskt startindex
+    normIndex = paramValue / scale;            //Hur många faktiska index skickar vi in från parameter
+    index = floor(normIndex);
+    return possibleStartPositions[index];
 }
 //EJ KONTROLLERAD
-function isEmpty(index){
-	if(matrix[index.i][index.j][index.k] = false) 
-		return false;
-	else return true;
+function isEmpty(index) {
+    if (matrix[index.i][index.j][index.k] = false)
+        return false;
+    else return true;
 }
 
 //EJ KONTROLLERAD!
 //True om ingen krock, uppdaterar matrisen 
 /*function noCollision(coord, rot){
-	var midCoord;
+    var midCoord;
 	
     var tempSegment = new THREE.CylinderGeometry(radiusTop, radiusBottom, lenght, 12);
     //Oklart om detta behövs, men har inte testat än
@@ -192,25 +192,25 @@ function isEmpty(index){
     seg.rotation.set(rot.x, rot.y, rot.z);
     
     tempSegment.TranslateY(segLen/2);
-	tempSegment.getWorldCoord(tempCoordinate);
+    tempSegment.getWorldCoord(tempCoordinate);
 	
     if(isEmpty(midCoord)){
-		var endCoord;
-		tempSegment.TranslateY(segLen/2);
-		tempSegment.getWorldCoord(endCoord);
+        var endCoord;
+        tempSegment.TranslateY(segLen/2);
+        tempSegment.getWorldCoord(endCoord);
 
-		if(isEmpty(endCoord)){
-			midCoord = coord2Index(midCoord);
+        if(isEmpty(endCoord)){
+            midCoord = coord2Index(midCoord);
             endCoord = coord2Index(temoCoord2);
             
             matrix[midCoord.x][midCoord.y][midCoord.z] = false;
-			matrix[endCoord.x][endCoord.y][endCoord.z] = false;
-			return true
-		}
-		else return false
-	} 
-	//OBS: Kan inte leta efter ny väg i noCollision eftersom väg-funktionen använder noCollision!
-	else return false
+            matrix[endCoord.x][endCoord.y][endCoord.z] = false;
+            return true
+        }
+        else return false
+    } 
+    //OBS: Kan inte leta efter ny väg i noCollision eftersom väg-funktionen använder noCollision!
+    else return false
 }*/
 
 
@@ -240,16 +240,16 @@ function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, sceneContainer.clientWidth / sceneContainer.clientHeight, 0.1, 1000);
     //camera = new THREE.OrthographicCamera(-5, 5, 5, -5, - 20, 1000);
-   // camera.position.z = 1;
+    // camera.position.z = 1;
     //camera.position.x = 0;
     //camera.position.y = 5;
     //camera.rotation.x = - Math.PI / 6;
     //camera.aspect = sceneContainer.clientWidth / sceneContainer.clientHeight;
     // camera.updateProjectionMatrix();
 
-   // camera.position.x = 9;
+    // camera.position.x = 9;
     camera.position.y = 15;
-   // camera.rotation.y = 0 * Math.PI / 2;
+    // camera.rotation.y = 0 * Math.PI / 2;
     //camera.position.y = 0.0;
 
     //camera.position.z = 10;
@@ -314,18 +314,16 @@ function setNewGirth(oldGirth, shrinkStep) {
 function createTreeSegment(pos, rot, char, lenght, bottomGirth) {
     //console.log("bottomGirth : " + bottomGirth)
 
-    var topGirth = setNewGirth(currentGirth, girthShrinkRates[girthShrinkRates.length-1])
+    var topGirth = setNewGirth(currentGirth, girthShrinkRates[girthShrinkRates.length - 1])
     //var topGirth = currentGirth - girthShrinkRates[girthShrinkRates.length-1]
     //console.log("topGirth : " + topGirth)
 
-    console.log("currentGirth : " + currentGirth)
-
     //console.log("calculating!!!!!!!!!!!!")
     //console.log(bottomGirth + " - " + girthShrinkRates[girthShrinkRates.length-1] + " = " + topGirth)
-    
+
     currentGirth = topGirth;
 
-    
+
 
     //var g = new THREE.CylinderGeometry(radiusTop, radiusBottom, lenght, 12);
     var g = new THREE.CylinderGeometry(currentGirth, bottomGirth, lenght, 12);
@@ -333,14 +331,14 @@ function createTreeSegment(pos, rot, char, lenght, bottomGirth) {
 
     var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));
     var m = new THREE.MeshLambertMaterial({ color: 0xFFCC00 });
-    
+
     m = getColor(char.instruction);
     var seg = new THREE.Mesh(g, m);
 
-//    var rand_rot = getDir(char);
+    //    var rand_rot = getDir(char);
     var rand_rot = Math.PI * char.rand;
 
-    if(current_index = 0) {rand_rot = 0}
+    if (current_index = 0) { rand_rot = 0 }
     //console.log(rand_rot)
 
     //seg.position.set(pos.x, pos.y, pos.z);
@@ -396,9 +394,9 @@ function getDepthOfSubtree(start_INDEX) {
 
     var depth = 0;
 
-    for(var i = start_INDEX+1; i < end_INDEX; i++) {
+    for (var i = start_INDEX + 1; i < end_INDEX; i++) {
 
-        if(sentence[i].instruction == "[") {
+        if (sentence[i].instruction == "[") {
             var endOfSideBranch = getEndIndexOfBranch(i)
             i = endOfSideBranch;
         }
@@ -424,7 +422,7 @@ function createTree(start_INDEX, end_INDEX, startPosition, c_rot, segLen, segGir
     //While we're not at the end of the local straight branch...
     while (current_index <= end_INDEX) {
         //Räknar...
-       
+
         if (sentence[current_index].instruction == "[") {
 
             var lbi = getEndIndexOfBranch(current_index); //Last bracket of new branch!
@@ -449,7 +447,7 @@ function createTree(start_INDEX, end_INDEX, startPosition, c_rot, segLen, segGir
             current_index = lbi; //Go to next index after recursive branch
 
         }
- 
+
 
         else if (sentence[current_index].instruction != "]" && sentence[current_index].instruction != "-" && sentence[current_index].instruction != "+") {
             createTreeSegment(rootCoord, c_rot, sentence[current_index], segmentLength, currentGirth);
@@ -468,13 +466,13 @@ function createTree(start_INDEX, end_INDEX, startPosition, c_rot, segLen, segGir
             girthStarts.pop()
             girthShrinkRates.pop()
         }
-        
- 
+
+
         ++current_index;
     }
     tempVec = dustyOldVec;
     rootCoord = oldPosition;
-   
+
 
 }
 
@@ -513,7 +511,7 @@ function generateScene(M, sphereRadie) {
 
 
     var subtreeDepth = getDepthOfSubtree(0)
-    girthShrinkRates.push((max_radiusBottom - min_radiusTop)/ subtreeDepth)
+    girthShrinkRates.push((max_radiusBottom - min_radiusTop) / subtreeDepth)
     girthStarts.push(max_radiusBottom)
 
 
@@ -526,23 +524,32 @@ function generateScene(M, sphereRadie) {
 
 
     createTree(0, sentence.length - 1, rootCoord, new THREE.Vector3(0, 0, 0), segmentLength, currentGirth);
-    
-    
+
+
+
+    //drawVoxel(new THREE.Vector3(0, 0, 0))
+    makeVoxelMatrix(10)
+
+
     renderer.render(scene, camera);
     document.getElementById("deleteButton").disabled = false;
+
+
+
+
 }
 
 const animate = () => {
     requestAnimationFrame(animate);
-  
+
     // Rotate the camera around the scene's origin
     const radius = 5;
     const angle = Date.now() * 0.0005;
     camera.position.x = radius * Math.cos(angle);
     camera.position.z = radius * Math.sin(angle);
-    camera.lookAt(0, 5, 0);
-  
+    camera.lookAt(0, 10, 0);
+
     renderer.render(scene, camera);
-  };
-  
-   animate();
+};
+
+animate();
